114ae81e55d4265470e86f0a91c700c5f4b68947
Brian Gulbis <brian.gulbis@gmail.com>
2019-02-11 15:55:01 -0600

    finalize data set

--- src/xx_tidy-data_epo_thuy.R
+++ src/xx_tidy-data_epo_thuy.R
@@ -41,5 +41,80 @@ hgb <- labs %>%
     ) %>%
     group_by(encounter_id, event_id) %>%
     summarize_at("lab_result", max, na.rm = TRUE) %>%
+    mutate(hgb_high = lab_result >= 10) %>%
+    rename(hgb = lab_result)
     
+iron_studies <- labs %>%
+    filter(lab != "Hgb") %>%
+    inner_join(
+        epo[c("encounter_id", "event_id", "clinical_event_datetime")],
+        by = "encounter_id"
+    ) %>%
+    filter(
+        !is.na(lab_result),
+        lab_datetime >= clinical_event_datetime - days(8),
+        lab_datetime <= clinical_event_datetime
+    ) %>%
+    distinct(encounter_id, event_id, lab, .keep_all = TRUE) %>%
+    select(encounter_id, event_id, lab, lab_result) %>%
+    spread(lab, lab_result) %>%
+    mutate(iron_studies = !is.na(`Ferritin Lvl`) | !is.na(Iron) | !is.na(TIBC) | !is.na(Transferrin) | !is.na(UIBC))
 
+transfuse <- prbc %>%
+    rename(prbc_event_id = event_id) %>%
+    inner_join(
+        epo[c("encounter_id", "event_id", "clinical_event_datetime")],
+        by = "encounter_id"
+    ) %>%
+    filter(
+        event_datetime >= clinical_event_datetime - days(8),
+        event_datetime <= clinical_event_datetime
+    ) %>%
+    group_by(encounter_id, event_id) %>%
+    summarize_at("event_datetime", min, na.rm = TRUE) %>%
+    rename(transfuse_datetime = event_datetime) %>%
+    mutate(transfused = !is.na(transfuse_datetime))
+
+iron_dose <- iron %>%
+    rename(med_event_id = event_id) %>%
+    mutate_at(
+        "route_event", 
+        str_replace_all, 
+        pattern = "IVPB",
+        replacement = "IV"
+    ) %>%
+    inner_join(
+        epo[c("encounter_id", "event_id", "clinical_event_datetime")],
+        by = "encounter_id"
+    ) %>%
+    filter(
+        med_datetime >= clinical_event_datetime - days(8),
+        med_datetime <= clinical_event_datetime
+    ) %>%
+    group_by(encounter_id, event_id, route_event) %>%
+    summarize_at("med_datetime", min, na.rm = TRUE) %>%
+    spread(route_event, med_datetime) %>%
+    mutate(iron_given = !is.na(IV) | !is.na(PO))
+    
+data_epo <- epo %>%
+    left_join(hgb, by = c("encounter_id", "event_id")) %>%
+    left_join(iron_studies, by = c("encounter_id", "event_id")) %>%
+    left_join(transfuse, by = c("encounter_id", "event_id")) %>%
+    left_join(iron_dose, by = c("encounter_id", "event_id")) %>%
+    mutate_at(
+        c("hgb_high", "iron_studies", "transfused", "iron_given"), 
+        funs(coalesce(., FALSE))
+    ) %>%
+    select(
+        fin,
+        encounter_type,
+        facility_event,
+        nurse_unit_event,
+        clinical_event_datetime,
+        dose,
+        route_event,
+        hgb_high,
+        iron_studies,
+        iron_given,
+        transfused
+    )
